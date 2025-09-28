ae594d0b041dc74fb9c5c8eeb5b03f70de5d572b
NzamaE <nzamaernest1@gmail.com>
2025-09-28 12:46:52 +0200

    Updated model/User on middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 // middleware/auth.js - Authentication middleware
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 
 const authenticateToken = async (req, res, next) => {
   try {
